20070c603296db781c4dc6aab3d80cb8473bcd81
favor-ui <fervplore@gmail.com>
2020-07-11 07:30:13 +0100

    saving config files on heroku

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-can-be-guest'\
 
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://favor:jaelec20@cluster0-ff1dc.mongodb.net/ikejaelect?retryWrites=true&w=majority'
+    MONGO_URI = os.environ.get('MONGO_URI') or os.environ.get("MONGO_URI")
 
 
     
