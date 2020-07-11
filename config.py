2949fc6d17346eef86aa77f90dd62832af4b38e2
favor-ui <fervplore@gmail.com>
2020-07-11 08:05:51 +0100

    removing  config files from heroku

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-can-be-guest'\
 
-    MONGO_URI = os.environ.get('MONGO_URI') or os.environ.get("MONGO_URI")
-
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://favor:jaelec20@cluster0-aet1e.mongodb.net/ikejaelect?retryWrites=true&w=majority'
 
     
