d168977b009f5440cd16ce893d1b5dab457c4c05
favor-ui <fervplore@gmail.com>
2020-07-11 10:12:13 +0100

    removing  config files from heroku

--- app/views.py
+++ app/views.py
@@ -80,64 +80,64 @@ def register():
 
 
 
-# @app.errorhandler(400)
-# def bad_request__error(exception):
-#     return jsonify(
-#         {
-#             "Message": "Sorry you entered wrong values kindly check and resend!"
-#         },
-#         {
-#             "status":400
-#         }
-#     )
+@app.errorhandler(400)
+def bad_request__error(exception):
+    return jsonify(
+        {
+            "Message": "Sorry you entered wrong values kindly check and resend!"
+        },
+        {
+            "status":400
+        }
+    )
 
 
-# @app.errorhandler(401)
-# def internal_error(error):
-#     return jsonify(
-#         {
-#             "Message": "Acess denied ! please register and login to generate API KEY"
-#         },
-#         {
-#             "status": 401
-#         }
-#     )
-
-
-
-# @app.errorhandler(404)
-# def not_found_error(error):
-#     return jsonify(
-#         {
-#             "Message":"Sorry the page your are looking for is not here kindly go back"
-#         },
-#         {
-#             "status": 404
-#         }
-#     )
+@app.errorhandler(401)
+def internal_error(error):
+    return jsonify(
+        {
+            "Message": "Acess denied ! please register and login to generate API KEY"
+        },
+        {
+            "status": 401
+        }
+    )
 
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify(
+        {
+            "Message":"Sorry the page your are looking for is not here kindly go back"
+        },
+        {
+            "status": 404
+        }
+    )
 
 
-# @app.errorhandler(405)
-# def method_not_allowed(error):
-#     return jsonify(
-#         {
-#             "Message": "Sorry the requested method is not allowed kindly check and resend !"
-#         },
-#         {
-#             "status": 405
-#         }
-#     )
 
-# @app.errorhandler(500)
-# def method_not_allowed(error):
-#     return jsonify(
-#         {
-#             "Message": "Bad request please check your input and resend !"
-#         },
-#         {
-#             "status": 500
-#         }
-#     )
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify(
+        {
+            "Message": "Sorry the requested method is not allowed kindly check and resend !"
+        },
+        {
+            "status": 405
+        }
+    )
+
+@app.errorhandler(500)
+def method_not_allowed(error):
+    return jsonify(
+        {
+            "Message": "Bad request please check your input and resend !"
+        },
+        {
+            "status": 500
+        }
+    )
