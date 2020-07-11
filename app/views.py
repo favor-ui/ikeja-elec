88fab832f62de205d236870a156663514c1e3880
favor-ui <fervplore@gmail.com>
2020-07-11 10:01:54 +0100

    removing  config files from heroku

--- app/views.py
+++ app/views.py
@@ -92,52 +92,52 @@ def register():
 #     )
 
 
-@app.errorhandler(401)
-def internal_error(error):
-    return jsonify(
-        {
-            "Message": "Acess denied ! please register and login to generate API KEY"
-        },
-        {
-            "status": 401
-        }
-    )
+# @app.errorhandler(401)
+# def internal_error(error):
+#     return jsonify(
+#         {
+#             "Message": "Acess denied ! please register and login to generate API KEY"
+#         },
+#         {
+#             "status": 401
+#         }
+#     )
 
 
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return jsonify(
-        {
-            "Message":"Sorry the page your are looking for is not here kindly go back"
-        },
-        {
-            "status": 404
-        }
-    )
+# @app.errorhandler(404)
+# def not_found_error(error):
+#     return jsonify(
+#         {
+#             "Message":"Sorry the page your are looking for is not here kindly go back"
+#         },
+#         {
+#             "status": 404
+#         }
+#     )
 
 
 
 
 
-@app.errorhandler(405)
-def method_not_allowed(error):
-    return jsonify(
-        {
-            "Message": "Sorry the requested method is not allowed kindly check and resend !"
-        },
-        {
-            "status": 405
-        }
-    )
+# @app.errorhandler(405)
+# def method_not_allowed(error):
+#     return jsonify(
+#         {
+#             "Message": "Sorry the requested method is not allowed kindly check and resend !"
+#         },
+#         {
+#             "status": 405
+#         }
+#     )
 
-@app.errorhandler(500)
-def method_not_allowed(error):
-    return jsonify(
-        {
-            "Message": "Bad request please check your input and resend !"
-        },
-        {
-            "status": 500
-        }
-    )
+# @app.errorhandler(500)
+# def method_not_allowed(error):
+#     return jsonify(
+#         {
+#             "Message": "Bad request please check your input and resend !"
+#         },
+#         {
+#             "status": 500
+#         }
+#     )
